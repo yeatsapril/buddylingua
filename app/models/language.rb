728933efd077f0d7d7db4bdf22c98c7453f9f729
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-10-06 15:53:34 +0200

    removed inclusion from language model

--- app/models/language.rb
+++ app/models/language.rb
@@ -1,6 +1,5 @@
 class Language < ApplicationRecord
-  LANGUAGE = %w[english spanish french german italian norwegian swedish danish polish lithuanian croatian russian].freeze
 
   has_many :users
-  validates :name, presence: true, inclusion: { in: LANGUAGE }
+  validates :name, presence: true
 end
