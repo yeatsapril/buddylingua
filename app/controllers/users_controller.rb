e22f6f3a95ee908e904d9a1355eda3e9875cca27
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-10-07 14:47:21 +0200

    edit and update now work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,26 @@ class UsersController < ApplicationController
 
   def update
     @user = User.find(params[:id])
+    @user.interests = []
 
+    params[:user][:interest_ids].delete_at(0)
+
+    params[:user][:interest_ids].each do |interest_id|
+      @user.interests << Interest.find(interest_id.to_i)
+    end
+
+    if @user.update(user_params)
+      redirect_to user_path(@user)
+    else
+      render :edit
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:description, :address, :age, :gender_id, :photo, :interest_ids)
   end
 
 end
