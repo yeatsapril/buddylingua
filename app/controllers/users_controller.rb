a6f7aac1b764dce77f106157c6ead38764de4665
Ingrid Pauchon <ingridpauchon@gmail.com>
2020-10-09 14:21:14 +0200

    sort by match percentage by default works now on index page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,16 +54,17 @@ class UsersController < ApplicationController
     @interests = Interest.order(:name)
     if session[:filter_option] && session[:filter] == "gender"
       @users = User.where(gender_id: session[:filter_option]) - current_user.buddies - [current_user]
+      @users = @users.sort_by { |user| -current_user.match_percentage(user) }
 
     elsif session[:filter_option] && session[:filter] == "interest"
       interest = Interest.find(session[:filter_option])
       @users = interest.users - current_user.buddies - [current_user]
-
+      @users = @users.sort_by { |user| -current_user.match_percentage(user) }
       # user_interests = UserInterest.where(interest_id: session[:filter_option])
       # @users = User.where(interest_id: session[:filter_option])
     else
       @users = User.all - current_user.buddies - [current_user]
-
+      @users = @users.sort_by { |user| -current_user.match_percentage(user) }
     end
   end
 
